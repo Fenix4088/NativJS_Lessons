333a57807206e0925dfefbb5867edcc18d8a6bc8
Yehor Pliasov <fenix4088@gmail.com>
2021-02-14 10:04:56 +0100

    Lesson 2 reqursion, stack, curry, closure

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,10 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './redux/state';
-
+import {b} from "./lessons/lesson2/lesson2";
+const lessons = {
+    lesson2: b
+}
 ReactDOM.render(
     <Provider store={store}>
         <App />
